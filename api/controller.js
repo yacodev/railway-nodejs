e8cb5aebd4422872356ffff4cd5461c86f04ef32
Carlos yaco <cyaco33@gmail.com>
2022-11-07 09:55:16 -0500

    intial config

---
+++ api/controller.js
@@ -0,0 +1,152 @@
+const request = require("request");
+const shippingMethods = require("../mock/shippingMethods");
+const coupons = require("../mock/coupons");
+const store = require("../store/db");
+
+const URL_BASE = "https://staging-apigw.getduna.com"; // Merchant API
+
+function merchantApi() {
+  async function getTokenizedOrder(orderPayload) {
+    const urlTokenizedOrder = `${URL_BASE}/merchants/orders`;
+
+    const response = await req("POST", urlTokenizedOrder, orderPayload);
+    //save data in DB
+    store.push("orders", {
+      token: response.token,
+      orderId: response.order.order_id,
+    });
+    return response;
+  }
+
+  async function getShippingMethods(orderId) {
+    const { order, token } = await getOrderWithToken(orderId);
+
+    // set shipping cost and modify total cost in order
+    order["shipping_amount"] = shippingMethods[0].cost;
+    order["sub_total"] = order["items_total_amount"] + order["shipping_amount"];
+    order["total_amount"] = order["sub_total"] + order["tax_amount"];
+
+    return {
+      order: order,
+      token: token,
+      shipping_methods: shippingMethods,
+    };
+  }
+
+  async function setShippingMethod(orderId, codeMethod) {
+    const { order, token } = await getOrderWithToken(orderId);
+
+    //modify shipping cost according new shipping method
+    let newShippingCost = 0;
+    for (const method in shippingMethods) {
+      const code = shippingMethods[method].code;
+      if (code === codeMethod) {
+        newShippingCost = shippingMethods[method].cost;
+      }
+    }
+    order["shipping_amount"] = newShippingCost;
+    order["sub_total"] = order["items_total_amount"] + newShippingCost;
+    order["total_amount"] = order["sub_total"] + order["tax_amount"];
+
+    return {
+      order: order,
+      token: token,
+    };
+  }
+
+  async function applyCoupon(orderId, couponCode) {
+    const { order, token } = await getOrderWithToken(orderId);
+
+    //validate coupon is available
+    const couponSelected = coupons.find((coupon) => coupon.code === couponCode);
+    if (!couponSelected) {
+      throw new Error("coupon not exist");
+    }
+
+    //apply  discount
+    order["sub_total"] = order["items_total_amount"] - couponSelected.amount;
+    order["total_amount"] =
+      order["sub_total"] + order["shipping_amount"] + order["tax_amount"];
+    //set coupon structure
+    order["discounts"] = couponSelected;
+
+    return {
+      order: order,
+      token: token,
+    };
+  }
+
+  async function removeCoupon(orderId, couponCode) {
+    const { order, token } = await getOrderWithToken(orderId);
+
+    //validate coupon is available
+    const couponSelected = coupons.find((coupon) => coupon.code === couponCode);
+    if (!couponSelected) {
+      throw new Error("coupon not exist");
+    }
+
+    //modify costs
+    order["sub_total"] = order["items_total_amount"] + couponSelected.amount;
+    order["total_amount"] =
+      order["sub_total"] + order["shipping_amount"] + order["tax_amount"];
+    //remove discounts
+    order["discounts"] = [];
+
+    return {
+      order: order,
+      token: token,
+    };
+  }
+
+  //get Order from Merchant API
+  async function getOrderWithToken(orderId) {
+    //get token from DB
+    const result = await store.get("orders", orderId);
+    if (!result) {
+      throw new Error("Data not found");
+    }
+    const token = result.token;
+    // get order from Merchant API
+    const urlOrderWithToken = `${URL_BASE}/merchants/orders/${token}`;
+    const response = await req("GET", urlOrderWithToken);
+    if (response.error) {
+      throw new Error("Error internal");
+    }
+    return response;
+  }
+
+  // Function to Request
+  function req(method, url, orderPayload) {
+    return new Promise((resolve, reject) => {
+      request(
+        {
+          method,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "X-API-KEY": process.env.DEUNA_PRIVATE_API_KEY,
+          },
+          url,
+          json: true,
+          body: orderPayload,
+        },
+        (err, req, body) => {
+          if (err) {
+            return reject(err.message);
+          }
+
+          return resolve(body);
+        }
+      );
+    });
+  }
+  return {
+    getTokenizedOrder,
+    getShippingMethods,
+    setShippingMethod,
+    applyCoupon,
+    removeCoupon,
+  };
+}
+
+module.exports = merchantApi();
---
+++ api/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "./.env" });
+const merchantApi = require("./controller");
+const response = require("../network/response");
+const orderPayload = require("../mock/orderPayload");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+// Get Tokenized Order
+app.post("/tokenizeOrder", (req, res) => {
+  merchantApi
+    .getTokenizedOrder(req.body)
+    .then((tokenizedOrder) => {
+      response.success(req, res, tokenizedOrder, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err, 400);
+    });
+});
+
+// Set ShippingMethods to Order
+app.post("/getShippingMethods/:orderId", (req, res) => {
+  merchantApi
+    .getShippingMethods(req.params.orderId)
+    .then((orderWithShippingMethods) => {
+      response.success(req, res, orderWithShippingMethods, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err, 400);
+    });
+});
+
+app.patch("/setShippingMethod/:orderId/:codeMethod", (req, res) => {
+  merchantApi
+    .setShippingMethod(req.params.orderId, req.params.codeMethod)
+    .then((orderWithShippingMethods) => {
+      response.success(req, res, orderWithShippingMethods, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err, 400);
+    });
+});
+
+app.post("/applyCoupons/:orderId", (req, res) => {
+  merchantApi
+    .applyCoupon(req.params.orderId, req.body.coupon_code)
+    .then((orderWithToken) => {
+      response.success(req, res, orderWithToken, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err, 400);
+    });
+});
+
+app.delete("/removeCoupons/:orderId/code/:couponCode", (req, res) => {
+  merchantApi
+    .removeCoupon(req.params.orderId, req.params.couponCode)
+    .then((orderWithToken) => {
+      response.success(req, res, orderWithToken, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err, 400);
+    });
+});
+app.listen(process.env.API_PORT, () => {
+  console.log("Node server listening on the port:", process.env.API_PORT);
+});
